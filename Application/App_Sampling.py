fa8bce32208f7838812d2167e44ba02efbb9a085
tsutakitenrin <2285814046@qq.com>
2024-11-06 20:14:28 +0800

    Update from MAC

--- Application/App_Sampling.py
+++ Application/App_Sampling.py
@@ -1,13 +1,25 @@
+import random
+
 from Tree_Sampling.Sampling import find_paths_and_collect, basic_sampling, basic_sampling_preprocess
+import random
+
+from Tree_Sampling.Sampling_Alias import alias_sampling, leaf_sampling_alias
+
 
 def calculate_records_num(canonical_nodes):
     s = 0
     for node in canonical_nodes:
         s += node.weight
     return s
-def sampling_application(root,values,left_idx,right_idx,k):
-    canonical, weights = find_paths_and_collect(root,values[left_idx],values[right_idx]) # Find the canonical nodes
+
+def sampling_application(root,left_val,right_val,k,df):
+    canonical, weights = find_paths_and_collect(root,left_val,right_val) # Find the canonical nodes
     total_records_num = calculate_records_num(canonical)
     basic_sampling_preprocess(canonical, weights)
-    result = basic_sampling(canonical, k) # Sampled K Nodes as the result
+    result = alias_sampling(canonical, k) # Sample a canonical node firstly from AS Sampling*
+    temp = []
+    for node in result:
+        res = leaf_sampling_alias(node) # Then use alias sampling to get the result
+        temp.append(res)
+    return temp,total_records_num
 
--- Application/Main.py
+++ Application/Main.py
@@ -1,18 +1,30 @@
+import pandas as pd
+
 from Application.Create_Data import generate, preprocess, weight_compute
 from Tree_Sampling.Construction_Tools import construct_bst, build_AS_structure, construct_bst_with_element
 from Tree_Sampling.Sample_Tools import calculate_weight, update_internal_nodes
+from Application.App_Sampling import sampling_application
 
 if __name__ == '__main__':
+    left_val = 20
+    right_val = 35
+    k = 5
     generate(100000,'data/data.csv')
+    df = pd.read_csv('data/data.csv')
     unique,values,grouped_id = preprocess('data/data.csv','Age')
     # group_id: {Age:[id1,id2,id3]}
     unique.sort()
     ls = []
     for u in unique:
         ls.append(u)
+
     weight_dict = sorted(weight_compute(values).items(),key = lambda x:x[0])
     elements = [item[0] for item in weight_dict]
     root,leaf_index = construct_bst_with_element(ls, elements, grouped_id)
     calculate_weight(root)
     update_internal_nodes(root)
     build_AS_structure(root)
+
+    # APPLICATION PART#
+    temp,total_num = sampling_application(root,left_val,right_val,k,df)
+    print(temp)
